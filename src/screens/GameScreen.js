40ed9843bba96ee8f0ed3f4419245c0e15527a5d
Nico Olivera <oliveran9@gmail.com>
2023-02-01 19:42:46 -0300

    el primer commit

---
+++ src/screens/GameScreen.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
+import Card from "../components/Card"
+
+import colors from "../constants/colors"
+
+const GameScreen = ({ handleResult }) => {
+  const [currentGuess, setCurrentGuess] = useState()
+
+  useEffect(() => {
+    setCurrentGuess(Math.floor(Math.random() * (99 - 1) + 1))
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textColor}>The opponent's assumption is:</Text>
+      <Text style={styles.textColor}>{currentGuess}</Text>
+      <Card newStyles={styles.buttonContainer}>
+        <Button
+          title="Lower"
+          onPress={() => handleResult("lower", currentGuess)}
+        />
+        <Button
+          title="Higher"
+          onPress={() => handleResult("higher", currentGuess)}
+        />
+      </Card>
+    </View>
+  )
+}
+
+export default GameScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+    backgroundColor: colors.primary,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+    width: "80%",
+  },
+  textColor: {
+    color: "#fff",
+  },
+})
